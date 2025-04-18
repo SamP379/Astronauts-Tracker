a124cf39a7025aced4a824bb03ebb25be2530547
Sam Patel <sampatel379@gmail.com>
2025-04-18 14:46:01 +0100

    Edited combine_paragraphs() function in scraper_utils.py

--- scraper_utils.py
+++ scraper_utils.py
@@ -1,18 +1,20 @@
 import bs4
 import requests
+import regex_utils
 
 
 def combine_paragraphs(soup : bs4.BeautifulSoup) -> str:
-    """Combines all parapgraph elements of a soup object into a string"""
-    paragraphs_text = ""
+    """Combines all paragraph elements of a soup object into a string"""
+    combined_text = ""
     paragraphs = soup.find_all("p")
     for p in paragraphs:
-        paragraphs_text += p.text
-    return paragraphs_text
+        clean_paragraph = regex_utils.remove_references(p.text)
+        combined_text += clean_paragraph
+    return combined_text
 
 
 def get_article_text(article_url : str) -> str|None:
-    """Gets the article text (paragraph elements) from a given article"""
+    """Gets the article text from a given article"""
     try:
         response = requests.get(url = article_url)
         raw_html = response.text
@@ -26,3 +28,4 @@ def get_article_text(article_url : str) -> str|None:
 # Testing
 WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/Matthew_Dominick"
 article_text = get_article_text(WIKIPEDIA_URL)
+print(article_text)
