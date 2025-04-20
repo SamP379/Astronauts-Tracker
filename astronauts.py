78ba67f2cc2b75eca8f901b496baf6f5217439c8
Sam Patel <sampatel379@gmail.com>
2025-04-20 13:50:14 +0100

    Created the Astronauts class in astronauts.py

--- astronauts.py
+++ astronauts.py
@@ -1,14 +1,27 @@
 import requests
 
 
-ASTRONAUTS_ENDPOINT = "http://api.open-notify.org/astros.json"
+class Astronauts:
+    """A class to represent the astronauts currently in space"""
+
+    ENDPOINT = "http://api.open-notify.org/astros.json"
+    
+    def __init__(self):
+        self.astronauts = self.get_astronauts()
 
 
-def get_astronauts() -> dict|None:
-    """Returns a dictionary of the astronauts currently in space"""
-    try:
-        response = requests.get(url = ASTRONAUTS_ENDPOINT)
-        content = response.json()
-        return content
-    except Exception:
-        return None
+    def get_astronauts(self) -> dict|None:
+        """Returns a dictionary of the astronauts currently in space"""
+        try:
+            response = requests.get(url = Astronauts.ENDPOINT)
+            content = response.json()
+            return content
+        except Exception:
+            return None
+    
+
+    def was_found(self):
+        """Returns True if the dictionary of astronauts was successfully loaded"""
+        if self.astronauts is not None:
+            return True
+        return False
