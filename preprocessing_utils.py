689752fe382c511e35cb43f89aa20db5384bb5f6
Sam Patel <sampatel379@gmail.com>
2025-04-20 18:36:28 +0100

    Added format_text() function to preprocessing_utils.py

--- preprocessing_utils.py
+++ preprocessing_utils.py
@@ -2,8 +2,16 @@ import re
 
 
 REFERENCES_PATTERN = r"\[\d*\]"
+NON_ALPHABET_PATTERN = r"[^a-zA-Z]"
+SPACES_PATTERN = r"\s+"
 
 
 def remove_references(text : str) -> str:
     new_text = re.sub(REFERENCES_PATTERN, "", text)
     return new_text
+
+
+def format_text(text : str) -> str:
+    formatted_text = re.sub(NON_ALPHABET_PATTERN, " ", text)
+    formatted_text = re.sub(SPACES_PATTERN, ' ', formatted_text)
+    return formatted_text
