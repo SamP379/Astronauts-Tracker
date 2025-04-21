dc9e3f0d11a19b32eed34d0aa878fff09693b2fc
Sam Patel <sampatel379@gmail.com>
2025-04-21 15:28:11 +0100

    Cleaned up code in main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ def main():
     astronauts = Astronauts()
     if astronauts.were_found():
         astronauts.display()
-        astronaut_name = input("Enter an astronaut name:")
+        print("")
+        astronaut_name = input("Enter an astronaut name: ").strip()
         if astronauts.check_astronaut(astronaut_name):
             astronaut_description = astronauts.get_description(astronaut_name)
             if astronaut_description is not None:
-                print(astronaut_description)
+                print(f"\n\nSummary:\n\n{astronaut_description}\n")
             else:
                 print(f"Unable to get a description for: {astronaut_name}")
         else:
