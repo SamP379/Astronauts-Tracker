aca5de02708a61095ba57db51499d933f6180750
Sam Patel <sampatel379@gmail.com>
2025-04-21 12:30:46 +0100

    CLeaned up code in nlp_utils.py by breaking code down into functions

--- nlp_utils.py
+++ nlp_utils.py
@@ -2,6 +2,9 @@ import re
 import nltk
 nltk.download('punkt_tab')
 
+
+STOPWORDS = nltk.corpus.stopwords.words('english')
+
  
 def preprocess_text(text : str) -> str:
     """Remove Wikipedia references, newlines, and extra whitespace from the text."""
@@ -18,6 +21,39 @@ def format_text(text : str) -> str:
     return formatted_text
 
 
+def calculate_word_frequencies(words_list : list) -> dict:
+    word_frequencies = {}
+    for word in words_list:
+        if word not in STOPWORDS:
+            if word not in word_frequencies:
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+    return word_frequencies
+
+
+def normalize_word_frequencies(word_frequencies : dict) -> dict:
+    maximum_frequncy = max(word_frequencies.values())
+    for word in word_frequencies:
+        word_frequencies[word] = word_frequencies[word] / maximum_frequncy
+    return word_frequencies
+
+
+def summarize_text(text : str) -> str:
+
+    clean_text = preprocess_text(article_text)
+    formatted_text = format_text(clean_text)
+
+    sentence_list = nltk.sent_tokenize(clean_text)
+
+    words_list = nltk.word_tokenize(formatted_text)
+    word_frequencies = calculate_word_frequencies(words_list)
+    word_frequencies = normalize_word_frequencies(word_frequencies)
+
+
+
+
+
 
 # --------------------- Testing code -------------------------- # 
 import scraper_utils
@@ -27,31 +63,4 @@ article_text = scraper_utils.article_text(url)
 if article_text is None:
     print("No article found")
     exit()
-# --------------------- Testing code -------------------------- # 
-
-
-clean_text = preprocess_text(article_text)
-formatted_text = format_text(clean_text)
-
-stopwords = nltk.corpus.stopwords.words('english')
-
-# Split text into a list of sentences and words
-sentence_list = nltk.sent_tokenize(clean_text)
-words_list = nltk.word_tokenize(formatted_text)
-
-# Find the frequency of occurence of each word
-word_frequencies = {}
-for word in words_list:
-    if word not in stopwords:
-        if word not in word_frequencies:
-            word_frequencies[word] = 1
-        else:
-            word_frequencies[word] += 1
-
-
-# Normalize frequencies 
-maximum_frequncy = max(word_frequencies.values())
-for word in word_frequencies:
-    word_frequencies[word] = word_frequencies[word] / maximum_frequncy
-
-print(word_frequencies)
+# --------------------- Testing code -------------------------- #
